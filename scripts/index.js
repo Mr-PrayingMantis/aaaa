2df24be1345594321fe48fa2693bac43a60ed721
Mr_PrayingMantis <giacomoandre.coppola@gmail.com>
2025-11-15 08:06:51 -0700

    update index.html index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,24 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+
+//const addCardFormElement = 
+
+const editProfileDescriptionInput = editProfileModal.querySelector("#profile-description-input");
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 
 editProfileBtn.addEventListener("click", function () {
+    editProfileNameInput.value = profileNameEl.textContent;
+    editProfileDescriptionInput.value = profileDescriptionEl.textContent;
     editProfileModal.classList.add("modal_is-open");
 });
 
@@ -23,3 +34,12 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
     newPostModal.classList.remove("modal_is-open");
 });
+
+function handleEditProfileSubmit(evt) {
+    evt.preventDefault();
+    profileNameEl.textContent = editProfileNameInput.value;
+    profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+    editProfileModal.classList.remove("modal_is-open");
+};
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
