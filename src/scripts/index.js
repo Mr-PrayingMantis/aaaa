d0c50a7863db6e82381dee2aaed4182c3dbac142
Mr_PrayingMantis <giacomoandre.coppola@gmail.com>
2026-04-03 16:12:42 -0700

    update all

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,32 +12,37 @@ const initialCards = [
     name: "Golden Gate Bridge",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
   },
+
   {
     name: "Val Thorens",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
+
   {
     name: "Restaurant terrace",
-    link: "./images/spots-images/2-photo-by-ceiline-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
+
   {
     name: "An outdoor cafe",
-    link: "./images/spots-images/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
+
   {
-    name: "A very long bridge, over the forest communist",
-    link: "./images/spots-images/4-photo-by-maurice-laschet-from-pexels.jpg",
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
 
   {
     name: "Tunnel with morning light",
-    link: "./images/spots-images/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
 
   {
     name: "Mountain house",
-    link: "./images/spots-images/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
+
 ];
 
 const editProfileBtn = document.querySelector(".profile__edit-btn");
